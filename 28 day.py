dd4828ea7893ced77054a57aadbe2e574428c673
SowmithraRavi <140725900+SowmithraRavi@users.noreply.github.com>
2024-02-04 20:35:55 +0530

    Union of Two Linked Lists.py

--- 28 day.py
+++ 28 day.py
@@ -18,4 +18,24 @@ class Solution:
                 t=t.next
         return h
 
+another solution:
+class Solution:
+    def union(self, head1,head2):
+        s=set()
+        temp1=head1
+        temp2=head2
+        while temp1:
+            s.add(temp1.data)
+            temp1=temp1.next
+        while temp2:
+            s.add(temp2.data)
+            temp2=temp2.next
+        s=list(sorted(s))
+        newnode=Node(-1)
+        temp=newnode
+        for i in s:
+            temp.next=Node(i)
+            temp=temp.next
+        return newnode.next
+
       
